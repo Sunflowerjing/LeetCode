e4259d75168414962a6f3ee960269f6b8304631e
qitmac001060 <qitmac001060@jingjingMacBookPro.local>
2019-09-06 11:07:53 +0800

    feat: 703的另一种写法

--- 703. 数据流中的第K大元素.js
+++ 703. 数据流中的第K大元素.js
@@ -12,6 +12,8 @@
  * 
  */
 
+
+ //  方法一
  /**
  * @param {number} k
  * @param {number[]} nums
@@ -48,6 +50,42 @@ KthLargest.prototype.add = function(val) {
     }
 };
 
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+
+
+  //  方法二
+  /**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k;
+    this.nums = nums.sort((a,b) => a-b);
+    
+};
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    let left = 0;
+    let right = this.nums.length;
+    while(left < right){
+       var mid = Math.floor((right+left)/2);
+       if(this.nums[mid] < val){
+          left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    this.nums.splice(left, 0 ,val);
+    return this.nums[this.nums.length - this.k];
+};
 /** 
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
